20cf67bb4db514cb03b4d95621302fd9cb72d9dd
Ryan Conway <ryan@rjc.cc>
2011-08-25 15:29:31 +0100

    Added :relative_path param to push(). Added :relative_rrd_path and :relative_output_path params to draw().

--- lib/impulse.rb
+++ lib/impulse.rb
@@ -7,29 +7,34 @@ module Impulse
     end
     
     def push(name, datas, params = {})
-      create_or_find_rrd(name, datas)
-      push_to_rrd(name, datas)
+      params = { 
+        :relative_path => '.',
+      }.merge(params)
+      create_or_find_rrd(name, datas, params)
+      push_to_rrd(name, datas, params)
     end
     
     def draw(name, time_scale = :day, params = {})
       params = { 
-        :filename => 'impuse.png', 
+        :filename => 'impulse.png',
+        :relative_rrd_path => '.',
+        :relative_output_path => '.', 
         :height => 200, 
         :width => 600, 
-        :title => 'Impuse Graph', 
+        :title => 'Impulse Graph', 
         :vertical => '',
         :legend => '',
         :thickness => 1
       }.merge(params)
       
       defaults = "-E -W 'Generated at #{Time.at(Time.now)}' -e now -h #{params[:height]} -w #{params[:width]}"
-      cmd = ["rrdtool graph #{params[:filename]} -s -#{seconds_for(time_scale)} #{defaults} -t '#{params[:title]}' -v '#{params[:vertical]}'"]
+      cmd = ["rrdtool graph #{params[:relative_output_path]}/#{params[:filename]} -s -#{seconds_for(time_scale)} #{defaults} -t '#{params[:title]}' -v '#{params[:vertical]}'"]
       
       if name.is_a?(Hash)
         c = 0
         name.each do |k,v|
           v = { :color => '#FF0000', :data => :data }.merge(v)
-          cmd << "DEF:d#{c}=#{v[:name]}.rrd:#{v[:data].to_s}:MAX"
+          cmd << "DEF:d#{c}=#{params[:relative_rrd_path]}/#{v[:name]}.rrd:#{v[:data].to_s}:MAX"
           cmd << "LINE#{params[:thickness]}:d#{c}#{v[:color]}:'#{k}' GPRINT:d#{c}:LAST:\"Last\\:%8.0lf\" GPRINT:d#{c}:MIN:\"	Min\\:%8.0lf\" GPRINT:d#{c}:AVERAGE:\"	Avg\\:%8.0lf\" GPRINT:d#{c}:MAX:\"	Max\\:%8.0lf\\n\""
           c += 1
         end
@@ -42,26 +47,26 @@ module Impulse
     end
     
     private
-    def create_or_find_rrd(name, datas)
+    def create_or_find_rrd(name, datas , params)
       averages = "RRA:AVERAGE:0.5:1:576 RRA:MIN:0.5:1:576 RRA:MAX:0.5:1:576 RRA:AVERAGE:0.5:6:432 RRA:MIN:0.5:6:432 RRA:MAX:0.5:6:432 RRA:AVERAGE:0.5:24:540 RRA:MIN:0.5:24:540 RRA:MAX:0.5:24:540 RRA:AVERAGE:0.5:288:450 RRA:MIN:0.5:288:450 RRA:MAX:0.5:288:450"
-      unless File.exist?(name + ".rrd")
+      unless File.exist?(params[:relative_path] + "/" + name + ".rrd")
         puts 'Creating RRD' if @debug
         if datas.is_a?(Hash)
           entries = []
           datas.each do |k,v|
             entries << "DS:#{k.to_s}:GAUGE:600:0:U"
           end
-          system "rrdtool create #{name}.rrd --step 300 --start #{Time.now.to_i - 1} #{entries.join(' ')} #{averages}"
+          system "rrdtool create #{params[:relative_path]}/#{name}.rrd --step 300 --start #{Time.now.to_i - 1} #{entries.join(' ')} #{averages}"
         else
-          system "rrdtool create #{name}.rrd --step 300 --start #{Time.now.to_i - 1} DS:data:GAUGE:600:0:U #{averages}"
+          system "rrdtool create #{params[:relative_path]}/#{name}.rrd --step 300 --start #{Time.now.to_i - 1} DS:data:GAUGE:600:0:U #{averages}"
         end
       end
     end
     
-    def push_to_rrd(name, data)
-      if File.exist?(name + ".rrd")
+    def push_to_rrd(name, data, params)
+      if File.exist?(params[:relative_path] + "/" + name + ".rrd")
         puts 'Updating RRD' if @debug
-        system "rrdtool update #{name}.rrd #{Time.now.to_i}:#{data}"
+        system "rrdtool update #{params[:relative_path]}/#{name}.rrd #{Time.now.to_i}:#{data}"
       end
     end
     
